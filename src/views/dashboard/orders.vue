477baef653f202b359a7af50ef59b0d8fcf0853b
Edmond GIHOZO <edmondgihozo@Edmonds-MacBook-Air.local>
2021-10-26 10:06:31 +0200

    orders page

--- src/views/dashboard/orders.vue
+++ src/views/dashboard/orders.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <v-card flat>
-      <v-card-title> Orders </v-card-title>
+      <v-card-title>Orders</v-card-title>
       <v-card-text>
         you can review and manage orders with their details.
       </v-card-text>
@@ -26,27 +26,33 @@
                   <h2>{{ item.tabContentName }} Orders</h2>
                   <v-divider class="mx-4" inset vertical></v-divider>
                   <v-spacer></v-spacer>
+
                   <v-dialog v-model="dialog" max-width="500px">
                     <v-card>
+                      <v-card-title>
+                        Edit Order
+                      </v-card-title>
                       <v-card-text>
                         <v-container>
                           <v-row>
-                            <v-col cols="12" sm="6" md="4">
-                              <v-text-field
-                                v-model="editedItem.status"
-                                label="Dessert name"
-                              ></v-text-field>
+                            <v-col cols="12">
+                              <v-select
+                                :items="status_types"
+                                label="Order Status"
+                              ></v-select>
                             </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                              <v-text-field
-                                v-model="editedItem.payment_method"
-                                label="Calories"
-                              ></v-text-field>
+                            <v-col cols="12">
+                              <v-select
+                                :items="status_types"
+                                label="Payment Method"
+                                disabled
+                              ></v-select>
                             </v-col>
-                            <v-col cols="12" sm="6" md="4">
+                            <v-col cols="12">
                               <v-text-field
                                 v-model="editedItem.delivery_date"
-                                label="Fat (g)"
+                                label="Delivery Date"
+                                type="datetime-local"
                               ></v-text-field>
                             </v-col>
                           </v-row>
@@ -64,38 +70,21 @@
                       </v-card-actions>
                     </v-card>
                   </v-dialog>
-                  <v-dialog v-model="dialogDelete" max-width="500px">
-                    <v-card>
-                      <v-card-title class="text-h5"
-                        >Are you sure you want to delete this
-                        item?</v-card-title
-                      >
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" text @click="closeDelete"
-                          >Cancel</v-btn
-                        >
-                        <v-btn
-                          color="blue darken-1"
-                          text
-                          @click="deleteItemConfirm"
-                          >OK</v-btn
-                        >
-                        <v-spacer></v-spacer>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
                 </v-toolbar>
               </template>
+              <!-- edit -->
               <template v-slot:item.actions="{ item }">
-                <v-icon small class="mr-2" @click="editItem(item)">
-                  mdi-pencil
-                </v-icon>
-                <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+                <v-btn color="primary" @click="editItem(item)" small depressed>
+                  <v-icon small class="mr-2">
+                    mdi-pencil
+                  </v-icon>
+                  Update
+                </v-btn>
               </template>
               <template v-slot:no-data>
-                <v-btn color="primary" @click="initialize"> Reset </v-btn>
+                <v-btn color="primary" @click="initialize">Reset</v-btn>
               </template>
+              <!-- end editing -->
             </v-data-table>
           </v-card>
         </v-tab-item>
@@ -106,60 +95,64 @@
 
 <script>
 export default {
-  name: "Orders",
+  name: 'Orders',
   watch: {
     dialog(val) {
-      val || this.close();
+      val || this.close()
     },
     dialogDelete(val) {
-      val || this.closeDelete();
+      val || this.closeDelete()
     },
   },
 
   created() {
-    this.initialize();
+    this.initialize()
   },
   data() {
     return {
       tab: null,
+      status_types: ['pending', 'completed', 'canceled'],
+      defaultDate: new Date(),
       filtertabs: [
         {
           id: 1,
-          title: "All orders",
-          tabContentName: "all",
+          title: 'All orders',
+          tabContentName: 'all',
         },
         {
           id: 2,
-          title: "Completed",
-          tabContentName: "completed",
+          title: 'Completed',
+          tabContentName: 'completed',
         },
         {
           id: 3,
-          title: "Pending",
-          tabContentName: "pending",
+          title: 'Pending',
+          tabContentName: 'pending',
         },
         {
           id: 4,
-          title: "Canceled",
-          tabContentName: "canceled",
+          title: 'Canceled',
+          tabContentName: 'canceled',
         },
       ],
       dialog: false,
       dialogDelete: false,
-      search: "",
+      search: '',
       headers: [
         {
-          text: "Order ID",
-          align: "start",
+          text: 'Order ID',
+          align: 'start',
           sortable: false,
-          value: "order_id",
+          value: 'order_id',
         },
-        { text: "Customer name", value: "customer.name" },
-        { text: "Delivery delivery address", value: "delivery_address" },
-        { text: "Delivery date", value: "delivery_date" },
-        { text: "Delivery Pricing", value: "delivery_price" },
-        { text: "Delivery status", value: "status" },
-        { text: "Payment Method", value: "payment", sortable: false },
+        { text: 'Customer name', value: 'customer.name' },
+        { text: 'Customer note', value: 'order_note' },
+        { text: 'Delivery delivery address', value: 'delivery_address' },
+        { text: 'Delivery date', value: 'delivery_date' },
+        { text: 'Delivery Pricing', value: 'delivery_price' },
+        { text: 'Delivery status', value: 'status' },
+        { text: 'Payment Method', value: 'payment', sortable: false },
+        { text: 'Actions', value: 'actions', sortable: false },
       ],
       orders: [],
       editedIndex: -1,
@@ -169,100 +162,92 @@ export default {
         payment_method: null,
       },
       defaultItem: {
-        name: "",
+        name: '',
         calories: 0,
         fat: 0,
         carbs: 0,
         protein: 0,
       },
-    };
+    }
   },
   methods: {
     initialize() {
       this.orders = [
         {
           customer: {
-            id: "423",
-            name: "kalisa john",
-            phones: "07834434534",
+            id: '423',
+            name: 'kalisa john',
+            phones: '07834434534',
           },
-          order_id: "2334df",
-          delivery_date: "12 / march 2021",
+          order_id: '2334df',
+          order_note: 'bring it when you are ready',
+          delivery_date: '2021-10-06T08:41',
           delivery_price: 34000,
-          delivery_address: "Kigali",
-          status: "pending",
-          payment: "Mobile Money",
+          delivery_address: 'Kigali',
+          status: 'pending',
+          payment: 'Mobile Money',
         },
         {
           customer: {
-            id: "23df3",
-            name: "g john",
-            phones: "07834434534",
+            id: '23df3',
+            name: 'g john',
+            phones: '07834434534',
           },
-          order_id: "2334df",
-          delivery_date: "12 / march 2021",
+          order_id: '2334df',
+          order_note: 'bring it when you are ready',
+          delivery_date: '2021-10-06T08:41',
           delivery_price: 34000,
-          delivery_address: "Kigali",
-          status: "pending",
-          payment: "Mobile Money",
+          delivery_address: 'Kigali',
+          status: 'pending',
+          payment: 'Mobile Money',
         },
         {
           customer: {
-            id: "2533",
-            name: "sa john",
-            phones: "07834434534",
+            id: '2533',
+            name: 'sa john',
+            phones: '07834434534',
           },
-          order_id: "2334df",
-          delivery_date: "12 / march 2021",
+          order_id: '2334df',
+          order_note: 'bring it when you are ready',
+          delivery_date: '2021-10-06T08:41',
           delivery_price: 34000,
-          delivery_address: "Kigali",
-          status: "pending",
-          payment: "Mobile Money",
+          delivery_address: 'Kigali',
+          status: 'pending',
+          payment: 'Mobile Money',
         },
-      ];
+      ]
     },
 
     editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
-
-    deleteItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
-    },
-
-    deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1);
-      this.closeDelete();
+      this.editedIndex = this.orders.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
     },
 
     close() {
-      this.dialog = false;
+      this.dialog = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
     },
 
     closeDelete() {
-      this.dialogDelete = false;
+      this.dialogDelete = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
     },
 
     save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+        Object.assign(this.orders[this.editedIndex], this.editedItem)
       } else {
-        this.desserts.push(this.editedItem);
+        this.orders.push(this.editedItem)
       }
-      this.close();
+      this.close()
     },
   },
-};
+}
 </script>
